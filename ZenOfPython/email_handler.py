fb14c4e3da1a5636a8d29e1e2300f880ee662049
Nwke <d.blinkovv@yandex.ru>
2019-02-04 16:40:41 +0300

    fix code style

--- ZenOfPython/email_handler.py
+++ ZenOfPython/email_handler.py
@@ -4,36 +4,37 @@ import imaplib
 from email.MIMEText import MIMEText
 from email.MIMEMultipart import MIMEMultipart
 
+DEFAULT_SMTP = 'smtp.gmail.com'
+DEFAULT_IMAP = 'imap.gmail.com'
+
 
 class MailHandler:
     letters_current_header = 0
     last_letter = -1
     raw_mail = 1
 
-    def __init__(self, login, password, recipients, subject, allowed_header, GMAIL_SMTP="smtp.gmail.com",
-                 GMAIL_IMAP="imap.gmail.com"):
+    def __init__(self, login, password, allowed_header, email_smtp=DEFAULT_SMTP,
+                 email_iap=DEFAULT_IMAP):
         self.login = login
         self.password = password
-        self.recipients = recipients
-        self.subject = subject
         self.allowed_header = allowed_header
+        self.email_smtp = email_smtp
+        self.email_iap = email_iap
 
-        self.GMAIL_SMTP = GMAIL_SMTP
-        self.GMAIL_IMAP = GMAIL_IMAP
-
-    def prepare_letter(self, message):
+    def prepare_letter(self, message, subject, recipients):
         our_letter = MIMEMultipart()
         our_letter['From'] = self.login
-        our_letter['To'] = ', '.join(self.recipients)
-        our_letter['Subject'] = self.subject
+        our_letter['To'] = ', '.join(recipients)
+        our_letter['Subject'] = subject
         our_letter.attach(MIMEText(message))
 
         return our_letter
 
-    def send_mail(self, message):
-        our_letter = self.prepare_letter(message)
+    def send_mail(self, message, subject, recipients):
+        our_letter = self.prepare_letter(message=message, subject=subject,
+                                         recipients=recipients)
 
-        mail_system = smtplib.SMTP(self.GMAIL_SMTP, 587)
+        mail_system = smtplib.SMTP(self.email_smtp, 587)
         # identify ourselves to smtp gmail client
         mail_system.ehlo()
         # secure our email with tls encryption
@@ -47,7 +48,7 @@ class MailHandler:
         mail_system.quit()  # send end
 
     def receive_mail(self):
-        mail_listening = imaplib.IMAP4_SSL(self.GMAIL_IMAP)
+        mail_listening = imaplib.IMAP4_SSL(self.email_iap)
         mail_listening.login(self.login, self.password)
         mail_listening.list()
         mail_listening.select("inbox")
@@ -56,26 +57,32 @@ class MailHandler:
         criterion = f'(HEADER Subject "{allowed_header}")'
 
         _, received_letters = mail_listening.uid('search', None, criterion)
-        assert received_letters[MailHandler.letters_current_header], 'There are no letters with current header'
+        assert received_letters([self.letters_current_header],
+                                'There are no letters with current header')
 
-        latest_email_uid = received_letters[MailHandler.letters_current_header].split()[MailHandler.last_letter]
-        _, received_letters = mail_listening.uid('fetch', latest_email_uid, '(RFC822)')
+        latest_email_uid = \
+            received_letters[self.letters_current_header].split()[
+                self.last_letter]
+        _, received_letters = mail_listening.uid('fetch', latest_email_uid,
+                                                 '(RFC822)')
 
-        raw_email = received_letters[MailHandler.letters_current_header][MailHandler.raw_mail]
+        raw_email = received_letters[self.letters_current_header][self.raw_mail]
         email_message = email.message_from_string(raw_email)
-        print(email_message)
+
         mail_listening.logout()  # end recieve
 
 
 if __name__ == '__main__':
-    login = 'login@gmail.com'
-    password = 'qwerty'
-    subject = 'Subject'
-    recipients = ['vasya@email.com', 'petya@email.com']
-    message = 'Message'
-    header = None
-    GMAIL_IMAP = "imap.gmail.com"
-    GMAIL_SMTP = "smtp.gmail.com"
+    def main():
+        login = 'login@gmail.com'
+        password = 'qwerty'
+        header = None
+        email_iap = "imap.gmail.com"
+        email_smtp = "smtp.gmail.com"
 
-    mail_handler = MailHandler(login=login, password=password, subject=subject, recipients=recipients, message=message,
-                               header=header, GMAIL_IMAP=GMAIL_IMAP, GMAIL_SMTP=GMAIL_SMTP)
+        mail_handler = MailHandler(login=login, password=password,
+                                   header=header, email_iap=email_iap,
+                                   email_smtp=email_smtp)
+
+
+    main()
