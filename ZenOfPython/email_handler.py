5afefe31686b7042f3995a195510e0f81b995213
Nwke <d.blinkovv@yandex.ru>
2019-02-07 21:51:47 +0300

    fix line length

--- ZenOfPython/email_handler.py
+++ ZenOfPython/email_handler.py
@@ -13,10 +13,10 @@ class MailHandler:
     last_letter = -1
     raw_mail = 1
 
-    def __init__(self, login, password, allowed_header, email_smtp=DEFAULT_SMTP, email_iap=DEFAULT_IMAP):
+    def __init__(self, login, password, email_smtp=DEFAULT_SMTP,
+                 email_iap=DEFAULT_IMAP):
         self.login = login
         self.password = password
-        self.allowed_header = allowed_header
         self.email_smtp = email_smtp
         self.email_iap = email_iap
 
@@ -30,7 +30,8 @@ class MailHandler:
         return our_letter
 
     def send_mail(self, message, subject, recipients):
-        our_letter = self.prepare_letter(message=message, subject=subject, recipients=recipients)
+        our_letter = self.prepare_letter(message=message, subject=subject,
+                                         recipients=recipients)
 
         mail_server = smtplib.SMTP(self.email_smtp, 587)
         mail_server.ehlo()
@@ -41,20 +42,25 @@ class MailHandler:
 
         mail_server.quit()
 
-    def receive_mail(self):
+    def receive_mail(self, allowed_header='ALL'):
         mail_listening = imaplib.IMAP4_SSL(self.email_iap)
         mail_listening.login(self.login, self.password)
         mail_listening.list()
         mail_listening.select("inbox")
 
-        allowed_header = self.allowed_header if self.allowed_header else 'ALL'
+        allowed_header = allowed_header if allowed_header is not None else 'ALL'
         criterion = f'(HEADER Subject "{allowed_header}")'
 
         _, received_letters = mail_listening.uid('search', None, criterion)
-        assert received_letters[self.letters_current_header], 'There are no letters with current header'
+        assert received_letters[
+            self.letters_current_header], 'There are no ' \
+                                          'letters with current header'
 
-        latest_email_uid = received_letters[self.letters_current_header].split()[self.last_letter]
-        _, received_letters = mail_listening.uid('fetch', latest_email_uid, '(RFC822)')
+        latest_email_uid = \
+            received_letters[self.letters_current_header].split()[
+                self.last_letter]
+        _, received_letters = mail_listening.uid('fetch', latest_email_uid,
+                                                 '(RFC822)')
 
         raw_email = received_letters[self.letters_current_header][self.raw_mail]
         email_message = email.message_from_bytes(raw_email)
@@ -70,11 +76,12 @@ def main():
     email_smtp = 'smtp.yandex.ru'
     email_iap = 'imap.yandex.ru'
 
-    mail_system = MailHandler(login=login, password=password, allowed_header=header, email_iap=email_iap,
-                              email_smtp=email_smtp)
+    mail_system = MailHandler(login=login, password=password,
+                              email_iap=email_iap, email_smtp=email_smtp)
 
-    mail_system.send_mail(message='Hello, world', subject='Test message', recipients=['d.blinkovv@yandex.ru'])
-    print(mail_system.receive_mail())
+    mail_system.send_mail(message='Hello, world', subject='Test message',
+                          recipients=['d.blinkovv@yandex.ru'])
+    print(mail_system.receive_mail(allowed_header=header))
 
 
 if __name__ == '__main__':
